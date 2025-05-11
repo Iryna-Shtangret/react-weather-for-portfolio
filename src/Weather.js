944deba3fe4ab883a62f5d503fd306bee14b0826
Iryna-Shtangret <113630738+Iryna-Shtangret@users.noreply.github.com>
2025-05-11 14:39:23 +0300

    Added CSS

--- src/Weather.js
+++ src/Weather.js
@@ -8,13 +8,17 @@ export default function Weather() {
           <div className="col-9">
             <input
               type="search"
-              placeholder="Enter a sity"
-              className="w-100"
+              placeholder="Enter a sity.."
+              className="inputSearch w-100"
               autoFocus
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="inputSubmit btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -25,19 +29,62 @@ export default function Weather() {
       </ul>
 
       <div className="row">
-        <div className="col-6 d-flex">
+        <div className="col-7 d-flex">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="icon weather"
           />
-          <div>21°C</div>
+          <div className="temperature">21</div>
+          <div className="unit">°C</div>
         </div>
-        <div className="col-6">
+        <div className="col-5">
           <ul>
             <li> Precipitation: 13%</li>
             <li> Humidity: 72%</li>
             <li> Wind: 19 km/h</li>
           </ul>
+        </div>
+      </div>
+      <div className="forecast d-flex justify-content-between">
+        <div className="text-center">
+          <div>Mon</div>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="icon forecast"
+          />
+          <div>25°C 18°C</div>
+        </div>
+        <div className="text-center">
+          <div>Mon</div>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="icon forecast"
+          />
+          <div>25°C 18°C</div>
+        </div>
+        <div className="text-center">
+          <div>Mon</div>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="icon forecast"
+          />
+          <div>25°C 18°C</div>
+        </div>
+        <div className="text-center">
+          <div>Mon</div>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="icon forecast"
+          />
+          <div>25°C 18°C</div>
+        </div>
+        <div className="text-center">
+          <div>Mon</div>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="icon forecast"
+          />
+          <div>25°C 18°C</div>
         </div>
       </div>
     </div>
