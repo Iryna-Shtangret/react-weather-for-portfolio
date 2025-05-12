291e75323c93bb64c538483be1bde8882e955da5
Iryna-Shtangret <113630738+Iryna-Shtangret@users.noreply.github.com>
2025-05-12 22:51:32 +0300

    Refresh API, added State

--- src/Weather.js
+++ src/Weather.js
@@ -1,109 +1,123 @@
+import { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 
-export default function Weather(props) {
+export default function Weather() {
+  //debugger;
+
+  const [temperature, setTemperature] = useState({ ready: false });
+  const [city, setCity] = useState("Kyiv");
+
   function refreshWeather(response) {
-    console.log(response.data);
-    alert(response.data.temperature.current);
+    console.log(response.data.temperature.current);
+    setTemperature({ ready: true, temp: response.data.temperature.current });
   }
 
-  function searchSity(city) {
+  function searchWeather() {
     let apikey = "dboef2d023e8b54bffat4b762d81356c";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
     axios.get(apiUrl).then(refreshWeather);
   }
 
-  function requestWeather(event) {
+  function search(event) {
     event.preventDefault();
-    searchSity(props.city);
+    searchWeather();
   }
 
-  return (
-    <div className="Weather">
-      <form onSubmit={requestWeather}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a sity.."
-              className="inputSearch w-100"
-              autoFocus
-            />
+  if (temperature.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={search}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a sity.."
+                className="inputSearch w-100"
+                name="inputFild"
+                autoFocus
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                name="submitBut"
+                className="inputSubmit btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="inputSubmit btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Tuesday 10:00</li>
-        <li>Fog</li>
-      </ul>
+        </form>
 
-      <div className="row">
-        <div className="col-7 d-flex">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="icon weather"
-          />
-          <div className="temperature">21</div>
-          <div className="unit">°C</div>
+        <h1>LvivLvivLviv</h1>
+        <ul>
+          <li>Tuesday 10:00</li>
+          <li>Fog</li>
+        </ul>
+
+        <div className="row">
+          <div className="col-7 d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="icon weather"
+            />
+            <div className="temperature">{temperature.temp}</div>
+            <div className="unit">°C</div>
+          </div>
+          <div className="col-5">
+            <ul>
+              <li> Precipitation: 13%</li>
+              <li> Humidity: 72%</li>
+              <li> Wind: 19 km/h</li>
+            </ul>
+          </div>
         </div>
-        <div className="col-5">
-          <ul>
-            <li> Precipitation: 13%</li>
-            <li> Humidity: 72%</li>
-            <li> Wind: 19 km/h</li>
-          </ul>
+        <div className="forecast d-flex justify-content-between">
+          <div className="text-center">
+            <div>Mon</div>
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="icon forecast"
+            />
+            <div>25°C 18°C</div>
+          </div>
+          <div className="text-center">
+            <div>Mon</div>
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="icon forecast"
+            />
+            <div>25°C 18°C</div>
+          </div>
+          <div className="text-center">
+            <div>Mon</div>
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="icon forecast"
+            />
+            <div>25°C 18°C</div>
+          </div>
+          <div className="text-center">
+            <div>Mon</div>
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="icon forecast"
+            />
+            <div>25°C 18°C</div>
+          </div>
+          <div className="text-center">
+            <div>Mon</div>
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="icon forecast"
+            />
+            <div>25°C 18°C</div>
+          </div>
         </div>
       </div>
-      <div className="forecast d-flex justify-content-between">
-        <div className="text-center">
-          <div>Mon</div>
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="icon forecast"
-          />
-          <div>25°C 18°C</div>
-        </div>
-        <div className="text-center">
-          <div>Mon</div>
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="icon forecast"
-          />
-          <div>25°C 18°C</div>
-        </div>
-        <div className="text-center">
-          <div>Mon</div>
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="icon forecast"
-          />
-          <div>25°C 18°C</div>
-        </div>
-        <div className="text-center">
-          <div>Mon</div>
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="icon forecast"
-          />
-          <div>25°C 18°C</div>
-        </div>
-        <div className="text-center">
-          <div>Mon</div>
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="icon forecast"
-          />
-          <div>25°C 18°C</div>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    searchWeather();
+    return "Loading...";
+  }
 }
